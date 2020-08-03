7d2a700c5fd9ddb0101d6f361e8eb3434207eae7
Michael Gallup <Gallup93@icloud.com>
2020-08-03 11:22:56 -0600

    Update testing with valid VCR cassette

--- spec/requests/api/v1/trail_requests_spec.rb
+++ spec/requests/api/v1/trail_requests_spec.rb
@@ -8,6 +8,9 @@ describe "expose trail end points" do
     expect(trail_response.status).to eq(200)
 
     trail_info = JSON.parse(trail_response.body, symbolize_names: true)
-    require "pry";binding.pry
+    attr_keys = [:temp, :weather, :location]
+    trail_keys = [:name, :summary, :difficulty, :location, :distance]
+    expect(trail_info[:attributes].keys).to eq(attr_keys)
+    expect(trail_info[:trails][0].keys).to eq(trail_keys)
   end
 end
