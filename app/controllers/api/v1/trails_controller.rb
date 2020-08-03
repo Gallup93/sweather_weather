0773917747bfebe236958817c72099e8a9d7bbb5
Michael Gallup <Gallup93@icloud.com>
2020-08-03 09:56:21 -0600

    Add route and start of index for trail controller

---
+++ app/controllers/api/v1/trails_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::TrailsController < ApplicationController
+  def index
+    forecast_json = get_forecast(get_coordinates)
+    parsed_forecast = parse_trail_forecast(forecast_json)
+    # forecast_object = Forecast.create(parsed_json)
+    require "pry";binding.pry
+  end
+
+  private
+
+  def parse_trail_forecast(forecast_json)
+    temp_weather = forecast_json[:current].extract!(:temp,:weather)
+    temp_weather[:weather] = temp_weather[:weather][0][:description]
+    temp_weather
+  end
+end
