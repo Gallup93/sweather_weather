70218b19ba8ddefd50af485817d79112de099970
Michael Gallup <Gallup93@icloud.com>
2020-08-04 09:54:44 -0600

    Refactor forecast_controller, Add helper POROs

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,42 +1,7 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    forecast_json = get_forecast(get_coordinates)
-    parsed_json = parse_forecast(forecast_json)
-    forecast_object = Forecast.create(parsed_json)
-    render json: ForecastSerializer.new(forecast_object)
+    CompileForecast.new(params["location"])
+    render json: ForecastSerializer.new(Forecast.last)
     Forecast.delete_all
-  end
-
-  private
-
-  def get_coordinates
-    location = params["location"].split(",")
-    MapquestServices.new.get_city_lat_lon(location[0], location[1])
-  end
-
-  def get_forecast(coordinates)
-    OpenweatherServices.new.get_forecast_by_lat_lng(coordinates)
-  end
-
-  def parse_forecast(forecast_json)
-    relevant_data = {:general => {}, :current => {}, :hourly => [], :daily => []}
-
-    tracker = nil
-
-    forecast_json.each do |key, value|
-      if relevant_data.keys.any?(key)
-        tracker = key
-      end
-      if tracker == nil
-        relevant_data[:general][key] = value
-      elsif tracker == :current
-        relevant_data[:current] = forecast_json[:current].extract!(:dt,:temp,:weather,:sunrise,:sunset,:feels_like,:humidity,:uvi,:visibility)
-      elsif tracker == :hourly
-        relevant_data[:hourly] = forecast_json[:hourly].map {|hour| hour.extract!(:temp,:weather,:dt)}
-      else
-        relevant_data[:daily] = forecast_json[:daily].map {|day| day.extract!(:temp,:weather,:rain)}
-      end
-    end
-    relevant_data
   end
 end
--- app/services/openweather_services.rb
+++ app/services/openweather_services.rb
@@ -4,8 +4,8 @@ class OpenweatherServices
     @conn = Faraday.new(url: "https://api.openweathermap.org/")
   end
 
-  def get_forecast_by_lat_lng(coordinates)
-    response = @conn.get("data/2.5/onecall?lat=#{coordinates[:lat]}&lon=#{coordinates[:lng]}&units=imperial&exclude=minutely&appid=#{ENV["OPENWEATHER_KEY"]}")
+  def get_forecast_by_lat_lng(lat,long)
+    response = @conn.get("data/2.5/onecall?lat=#{lat}&lon=#{long}&units=imperial&exclude=minutely&appid=#{ENV["OPENWEATHER_KEY"]}")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
