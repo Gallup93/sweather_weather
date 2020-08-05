25a897ca47a28554f630c38c6f4c03c3f7caab70
Michael Gallup <Gallup93@icloud.com>
2020-08-04 18:56:07 -0600

    Add find_user_by_api_key helper in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::API
   def  find_user_by_email(email)
     User.find_by(email: email)
   end
+  def  find_user_by_api_key(key)
+     User.find_by(api_key: key)
+  end
 end
