92568ee499f9ae2a233ed1817be9540dd8e9b7a7
Michael Gallup <Gallup93@icloud.com>
2020-08-04 18:55:25 -0600

    Add RoadTripController

---
+++ app/controllers/api/v1/road_trips_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::RoadTripsController < ApplicationController
+  def create
+    user = find_user_by_api_key(params["api_key"])
+    if user
+      response = CompileTripData.new(params.extract!("origin","destination"))
+      render json: response
+    else
+      render json: ["unauthorized"], status: :unauthorized
+    end
+
+  end
+end
